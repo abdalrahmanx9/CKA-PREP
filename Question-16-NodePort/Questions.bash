ef330acd0765ce5e82a3f9fccb62c7c7c80ff8fd
abdulrahman <ug_31107079@ics.tanta.edu.eg>
2026-09-17 18:43:47 +0300

    Q16: rewrite question in exam style

--- Question-16-NodePort/Questions.bash
+++ Question-16-NodePort/Questions.bash
@@ -1,10 +1,15 @@
-Question
-There is a deployment named nodeport-deployment in the relative namespace
+Question: NodePort
 
-Tasks:
-1. Configure the deployment so it can be exposed on port 80, name=http, protocol TCP
-2. Create a new Service named nodeport-service exposing the container port 80, protocol TCP, NodePort 30080
-3. Configure the new Service to also expose the individual pods using NodePort
+A deployment named nodeport-deployment is running in the namespace relative.
+
+Task:
+Create a Service named nodeport-service in the relative namespace to expose the deployment nodeport-deployment, with the following requirements:
+- The Service must be of type NodePort
+- It must be reachable on node port 30080
+- The container port of the deployment is named http, exposed on port 80 with protocol TCP
+- All pods of the deployment must be included as endpoints
+
+Verify that the deployment is reachable via curl http://<nodeIP>:30080
 
 Video Link
 https://youtu.be/t1FxX3PmYDQ?si=ryASL-G9X2FCVApQ
