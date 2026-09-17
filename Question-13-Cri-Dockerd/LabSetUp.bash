18a7becd7b088e1e630517e51a799fee3673feea
abdulrahman <ug_31107079@ics.tanta.edu.eg>
2026-09-17 17:52:33 +0300

    Q13: download cri-dockerd.deb to /root to match question

--- Question-13-Cri-Dockerd/LabSetUp.bash
+++ Question-13-Cri-Dockerd/LabSetUp.bash
@@ -2,7 +2,8 @@
 set -e
 
 # Download CRI Dockerd Debian package
-wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.20/cri-dockerd_0.3.20.3-0.debian-bullseye_amd64.deb -O /tmp/cri-dockerd.deb
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.20/cri-dockerd_0.3.20.3-0.debian-bullseye_amd64.deb -O /root/cri-dockerd.deb
 
-echo "CRI Dockerd package downloaded to /tmp/cri-dockerd.deb"
+echo "CRI Dockerd package downloaded to /root/cri-dockerd.deb"
+
 
